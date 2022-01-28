22cc496007a3c5a2277b458bfdcda22c7de8a758
ehsanodyssey <e.shahmirzaloo@kian.digital>
2022-01-28 23:02:44 +0330

    add properties files based on maven profiles

---
+++ src/main/java/ir/snapp/pay/billsharing/BillSharingApp.java
@@ -0,0 +1,17 @@
+package ir.snapp.pay.billsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:ehsan.odyssey@gmail.com">EhsanOdyssey</a>
+ * @project snaplitto
+ * @date Fri 28 Jan 2022
+ */
+@SpringBootApplication
+public class BillSharingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillSharingApp.class, args);
+    }
+}
